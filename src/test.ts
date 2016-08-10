a3ec52db729de96c0852647380ee23f893f45d60
hashrock <hashedrock@gmail.com>
2016-08-11 00:57:21 +0900

    Add a test for the parser

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,19 @@
 /// <reference path="../node_modules/@types/mocha/index.d.ts" />
+import {Task, parseTask} from "./lib"
 
 import {assert} from "chai";
 
-describe("module1 default function", () => {
-    it("returns module1", () => {
-        //assert.equal(sum(1,2), 3);
+describe("parseTask", () => {
+    it("タスク一つの場合のパース", (done) => {
+        let tasks: Task[] = parseTask("task1 2016-01-01 2016-02-01")
+        assert.equal(tasks.length, 1)
+        assert.deepEqual(
+          tasks[0], {
+            name: "task1",
+            start: new Date("2016-01-01").getTime(),
+            end: new Date("2016-02-02").getTime() //終了日は実際には翌日の00:00になる
+          }
+        )
+        done()
     });
 });
