6e92735c9fa64402e279ab38da7430c25f85d9fc
hashrock <hashedrock@gmail.com>
2016-08-11 02:02:24 +0900

    Support range start and end

--- src/main.ts
+++ src/main.ts
@@ -21,17 +21,27 @@ class GanttDown {
     this.ctx.stroke();
   }
 
-  private drawMonthLabel(ctx: CanvasRenderingContext2D, month: number){
+  private drawMonthLabel(ctx: CanvasRenderingContext2D, start: moment.Moment, dayNum: number, size: number){
+    var month = start.month();
     this.ctx.save();
     this.ctx.font = "24px 'sans-serif'";
-    this.ctx.fillText("" + (month + 1) + "月", 4, 24);
+    this.ctx.fillText("" + (month+1) + "月", 0, 24);
+    var _start = start.clone()
+    for (var i = 0; i < dayNum; i++) {
+      if(_start.month() > month){
+        month = _start.month();
+        this.ctx.fillText("" + (month+1) + "月", i * size, 24);
+      }
+      _start.add(1, "days")
+    }
     this.ctx.restore();
   }
 
-  private drawWeekdayBackground(ctx: CanvasRenderingContext2D, dayNum: number, size: number, paddingTop: number){
+  private drawWeekdayBackground(ctx: CanvasRenderingContext2D, start: moment.Moment, dayNum: number, size: number, paddingTop: number){
+    var _start = start.clone()
     ctx.save()
     for (var i = 0; i < dayNum; i++) {
-      var t = moment().date(i + 1).weekday()
+      var t = _start.weekday()
       var color = "#FFFFFF";
       if(t === 0){
         color = "#FFDDDD"
@@ -41,31 +51,40 @@ class GanttDown {
       }
       ctx.fillStyle = color
       ctx.fillRect(i * size, paddingTop, size, this.h - paddingTop)
+      _start.add(1, "days")
     }
     ctx.restore();
   }
 
-  private drawVerticalLines(ctx: CanvasRenderingContext2D, dayNum: number, size: number, paddingTop: number){
+  private drawVerticalLines(ctx: CanvasRenderingContext2D, start: moment.Moment, dayNum: number, size: number, paddingTop: number){
     this.ctx.save()
+    var _start = start.clone()
     for (var i = 0; i < dayNum; i++) {
       this.ctx.font = "12px 'sans-serif'"
       this.ctx.textAlign = "center"
-      this.ctx.fillText(""+ (i+1), i * size + size / 2, paddingTop)
+      this.ctx.fillText(""+ _start.date(), i * size + size / 2, paddingTop)
       this.line(i * size, paddingTop, i * size, this.h)
+      _start.add(1, "days")
     }
     this.ctx.restore()
   }
 
-
   redraw() {
+    var days = 36;
+
+    var start = moment();
+    start.set({hour:0,minute:0,second:0,millisecond:0});
+
+    var end = moment().add(days, "days");
+    end.set({hour:0,minute:0,second:0,millisecond:0});
+
     var month:number = moment().month();
-    var day = moment().daysInMonth()
-    var size = this.w / day
+    var size = this.w / days
     var paddingTop = 48
 
-    this.drawMonthLabel(this.ctx, month)
-    this.drawWeekdayBackground(this.ctx, day, size, paddingTop)
-    this.drawVerticalLines(this.ctx, day, size, paddingTop)
+    this.drawMonthLabel(this.ctx, start, days, size)
+    this.drawWeekdayBackground(this.ctx, start, days, size, paddingTop)
+    this.drawVerticalLines(this.ctx, start, days, size, paddingTop)
   }
 }
 
